74706ebfbb12633db44672f6e171672f10b658fa
唐万龙 <tangwanlong425@qq.com>
2017-06-14 23:03:34 +0800

    注入

--- LOJSBridge-OC/LOJSBridge-OC/ViewController.m
+++ LOJSBridge-OC/LOJSBridge-OC/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 #import "LOJSBridge.h"
 
-@interface ViewController ()<UIWebViewDelegate>
+@interface ViewController ()<UIWebViewDelegate> {
+    LOJSBridge *_loJSBridge;
+}
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
@@ -24,28 +26,33 @@
     [self.webView loadRequest:request];
     
     self.webView.delegate = self;
+    
+    
+    _loJSBridge = [LOJSBridge instanceWithVarName:@"iOSNative"];
+    
 }
 
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
     
+    if ([_loJSBridge handleRequestString:webView.request.URL.absoluteString]) return NO;
     
     return YES;
 }
 
 
 - (void)webViewDidStartLoad:(UIWebView *)webView {
-    
+    [_loJSBridge injectStartJSIn:webView];
 }
 
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
-    
+    [_loJSBridge injectFinishJSIn:webView];
 }
 
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
-    
+
 }
 
 
