e9f8c6e61c7b13eaa82e42e74d2805a222dc382b
唐万龙 <tangwanlong425@qq.com>
2017-06-14 16:20:17 +0800

    第一版

---
+++ LOJSBridge-OC/LOJSBridge-OC/LOJSBridge/LOJSBridge.h
@@ -0,0 +1,53 @@
+//
+//  LOJSBridge.h
+//  LOJSBridge-OC
+//
+//  Created by 唐万龙 on 2017/6/14.
+//  Copyright © 2017年 唐万龙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger,InjectionType) {
+    InjectionTypeStart = 0,  //在网页开始加载时注入
+    InjectionTypeFinish = 1  //在网页加载结束后注入
+};
+
+@interface LOJSBridge : NSObject
+
+/**
+ 初始化
+ 
+ @param varname 变量名 windows.native 则为native
+ @return instancetype
+ */
++ (instancetype _Nonnull)instanceWithVarName:(NSString *_Nonnull)varname;
+
+/**
+ 添加JS与iOS方法
+ 
+ @param name JS方法名 Windows.heikuai.close 为 close
+ @param action iOS 方法
+ */
+- (void)addJSFunctionName:(NSString * _Nonnull)name target:(id _Nonnull )target selector:(SEL _Nonnull)action type:(InjectionType)type;
+
+
+
+/**
+ 添加返回参数的JS
+ 
+ @param name 方法名
+ @param value 返回值
+ */
+- (void)addReturnJSFunctionName:(NSString *_Nonnull )name value:(id _Nonnull)value type:(InjectionType)type;
+
+
+/**
+ 处理Web内部请求，映射到 iOS 方法中
+ _Nonnull
+ @param string 请求地址，本类内部已定义
+ */
+- (BOOL)handleRequestString:(NSString *_Nonnull)string;
+
+
+@end
---
+++ LOJSBridge-OC/LOJSBridge-OC/LOJSBridge/LOJSBridge.m
@@ -0,0 +1,171 @@
+//
+//  LOJSBridge.m
+//  LOJSBridge-OC
+//
+//  Created by 唐万龙 on 2017/6/14.
+//  Copyright © 2017年 唐万龙. All rights reserved.
+//
+
+#import "LOJSBridge.h"
+
+@interface LOJSBridge () {
+    NSString *_varName;
+    NSMutableDictionary *_targetDict;
+    NSMutableDictionary *_selDict;
+}
+
+
+@property (nonatomic, copy) NSString *jsStartString;
+@property (nonatomic, copy) NSString *jsFinishString;
+
+@end
+
+@implementation LOJSBridge
+
+
++ (instancetype)instanceWithVarName:(NSString *)varname {
+    return [[self alloc] initWithVarName:varname];
+}
+
+- (instancetype)initWithVarName:(NSString *)var {
+    _varName = var;
+    _jsStartString = [NSString stringWithFormat:@"var %@={};",_varName];
+    _jsFinishString = @"";
+    _targetDict = [NSMutableDictionary dictionary];
+    _selDict = [NSMutableDictionary dictionary];
+    return self;
+}
+
+- (void)addJSFunctionName:(NSString *)name target:(id)target selector:(SEL)action type:(InjectionType)type {
+    //缓存
+    [_targetDict setObject:target forKey:name];
+    [_selDict setObject:NSStringFromSelector(action) forKey:name];
+    
+    //判断iOS方法参数个数
+    NSArray *param = [NSStringFromSelector(action) componentsSeparatedByString:@":"];
+    
+    NSString *jsParamString = @"";   //JS方法中的参数格式
+    NSString *urlParamString = @"";  //连接中的参数格式
+    if (param.count - 1) {
+        //有参数
+        urlParamString = @"+";
+        for (int i = 0; i < param.count - 1; i++) {
+            if (i != 0) {
+                jsParamString = [jsParamString stringByAppendingString:@","];
+                urlParamString = [urlParamString stringByAppendingString:@"+':://:'+"];
+            }
+            
+            NSString *param = [NSString stringWithFormat:@"parameter%d",i + 1];
+            
+            jsParamString = [jsParamString stringByAppendingString:param];
+            
+            
+            urlParamString = [urlParamString stringByAppendingString:param];
+        }
+        
+        NSString *actionJS = [NSString stringWithFormat:@"%@.%@=function(%@){window.location.href='iossel://///%@:///:'%@};",_varName,name,jsParamString,name,urlParamString];
+        
+        if (type == InjectionTypeStart) {
+            self.jsStartString = [self.jsStartString stringByAppendingString:actionJS];
+        } else if (type == InjectionTypeFinish) {
+            self.jsFinishString = [self.jsFinishString stringByAppendingString:actionJS];
+        }
+        
+    } else{
+        //无参数
+        NSString *actionJS = [NSString stringWithFormat:@"%@.%@=function(%@){window.location.href='iossel://///%@'};",_varName,name,jsParamString,name];
+        
+        if (type == InjectionTypeStart) {
+            self.jsStartString = [self.jsStartString stringByAppendingString:actionJS];
+        } else if (type == InjectionTypeFinish) {
+            self.jsFinishString = [self.jsFinishString stringByAppendingString:actionJS];
+        }
+    }
+}
+
+
+- (void)addReturnJSFunctionName:(NSString *)name value:(id)value type:(InjectionType)type {
+    NSString *actionJS = [NSString stringWithFormat:@"%@.%@=function(){return '%@'}",_varName,name,value];
+    if (type == InjectionTypeStart) {
+        self.jsStartString = [self.jsStartString stringByAppendingString:actionJS];
+    } else if (type == InjectionTypeFinish) {
+        self.jsFinishString = [self.jsFinishString stringByAppendingString:actionJS];
+    }
+}
+
+
+- (BOOL)handleRequestString:(NSString *)string {
+    if ([string hasPrefix:@"iossel://///"]) {
+        NSString *actionString = [[string componentsSeparatedByString:@"/////"] lastObject];
+        
+        NSArray *actionParamList = [actionString componentsSeparatedByString:@":///:"];
+        
+        NSString *actionName = [actionParamList firstObject];
+        //方法名
+        
+        id target = [_targetDict objectForKey:actionName];
+        SEL selector = NSSelectorFromString([_selDict objectForKey:actionName]);
+        
+        NSArray *params;
+        if (actionParamList.count > 1) {
+            //有参数
+            params = [[actionParamList lastObject] componentsSeparatedByString:@":://:"]; //iOS方法的参数
+        } else {
+            //无参数
+            params = @[];
+        }
+        
+        if ([target respondsToSelector:selector]) {
+            [self performtarget:target selector:selector withObjects:params];
+        }
+        
+        return YES;
+        
+    } else {
+        
+        return NO;
+    }
+}
+
+
+
+
+
+- (id)performtarget:(id)target selector:(SEL)selector withObjects:(NSArray *)args {
+    NSMethodSignature *sig = [target methodSignatureForSelector:selector];
+    
+    if (sig) {
+        NSInvocation* invo = [NSInvocation invocationWithMethodSignature:sig];
+        
+        [invo setTarget:target];
+        
+        [invo setSelector:selector];
+        
+        //设置参数
+        for (int i = 0; i < args.count; i++) {
+            id obj = args[i];
+            [invo setArgument:&obj atIndex:i + 2];
+            
+        }
+        
+        [invo invoke];
+        
+        
+        if (sig.methodReturnLength) {
+            
+            id anObject;
+            
+            [invo getReturnValue:&anObject];
+            
+            return anObject;
+        }  else {
+            return nil;
+        }
+    } else {
+        return nil;
+    }
+}
+
+
+
+@end
